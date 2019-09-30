7e6c79748bd39ebb7b91bc0ca44fbef348353156
Stian Tofte <stitofte@outlook.com>
2019-10-01 00:45:15 +0200

    Cooldown implemented.

--- src/main/java/com/ohneemc/OhWild/WildCommand.java
+++ src/main/java/com/ohneemc/OhWild/WildCommand.java
@@ -1,13 +1,19 @@
 package com.ohneemc.OhWild;
 
+import com.ohneemc.OhWild.util.CooldownManager;
 import com.ohneemc.OhWild.util.CreateInventory;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 
+import java.util.concurrent.TimeUnit;
+
 public class WildCommand implements CommandExecutor {
+
+    private final CooldownManager cooldownManager = new CooldownManager();
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -15,7 +21,14 @@ public class WildCommand implements CommandExecutor {
             Player player = ((Player) sender).getPlayer();
             Inventory inv = CreateInventory.createWildInventory(player);
             if (player != null && inv != null) {
-                player.openInventory(inv);
+                long timeLeft = System.currentTimeMillis() - cooldownManager.getCooldown(player.getUniqueId());
+                if (TimeUnit.MILLISECONDS.toSeconds(timeLeft) > CooldownManager.DEFAULT_COOLDOWN || CooldownManager.DEFAULT_COOLDOWN == -1){
+                    cooldownManager.setCooldown(player.getUniqueId(), System.currentTimeMillis());
+                    player.openInventory(inv);
+                }else{
+                    long seconds = (TimeUnit.MILLISECONDS.toSeconds(timeLeft) - CooldownManager.DEFAULT_COOLDOWN);
+                    player.sendMessage(ChatColor.GOLD + String.valueOf(seconds) + ChatColor.GREEN + " seconds before you can use wild again.");
+                }
                 return true;
             }
         }
---
+++ src/main/java/com/ohneemc/OhWild/util/CooldownManager.java
@@ -0,0 +1,24 @@
+package com.ohneemc.OhWild.util;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class CooldownManager {
+
+    private final Map<UUID, Long> cooldowns = new HashMap<>();
+
+    public static final int DEFAULT_COOLDOWN = Config.getInteger("settings.cooldown");
+
+    public void setCooldown(UUID player, long time){
+        if(time < 1) {
+            cooldowns.remove(player);
+        } else {
+            cooldowns.put(player, time);
+        }
+    }
+
+    public long getCooldown(UUID player){
+        return cooldowns.getOrDefault(player, 0L);
+    }
+}
