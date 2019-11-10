dd9e0b72583cc5275552630594781ff4196242ac
Stian Tofte <stitofte@outlook.com>
2019-11-11 00:06:04 +0100

    Added cooldown pr world map.

--- src/main/java/com/ohneemc/OhWild/util/Maps.java
+++ src/main/java/com/ohneemc/OhWild/util/Maps.java
@@ -5,4 +5,5 @@ import java.util.HashMap;
 public class Maps {
 
     public static HashMap<String, String> itemWorld = new HashMap<>();
+    public static HashMap<String, Integer> worldCooldown = new HashMap<>();
 }
