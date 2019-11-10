4b7c90dc1c936727432aea928385cb5f343eef3d
Stian Tofte <stitofte@outlook.com>
2019-11-10 23:55:36 +0100

    Added check for cooldown pr world.

--- src/main/java/com/ohneemc/OhWild/WildCommand.java
+++ src/main/java/com/ohneemc/OhWild/WildCommand.java
@@ -15,6 +15,7 @@ import java.util.concurrent.TimeUnit;
 public class WildCommand implements CommandExecutor {
 
     private final String cooldownMessage = Config.getString("messages.cooldown");
+    private final boolean DEFAULT_PRWORLD = Config.getBoolean("settings.prworld");
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -23,17 +24,22 @@ public class WildCommand implements CommandExecutor {
             Inventory inv = CreateInventory.createWildInventory(player);
             if (player != null && inv != null) {
                 long timeLeft = System.currentTimeMillis() - CooldownManager.getCooldown(player.getUniqueId());
-                if (TimeUnit.MILLISECONDS.toSeconds(timeLeft) > CooldownManager.DEFAULT_COOLDOWN || CooldownManager.DEFAULT_COOLDOWN == -1){
+
+                if (DEFAULT_PRWORLD){
                     player.openInventory(inv);
                 }else{
-                    if (cooldownMessage.length() > 1) {
-                        long seconds = (TimeUnit.MILLISECONDS.toSeconds(timeLeft) - CooldownManager.DEFAULT_COOLDOWN);
-                        String strip = String.valueOf(seconds).replace("-", "");
-                        String message = cooldownMessage.replace("{time}", strip);
-                        player.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
+                    if (TimeUnit.MILLISECONDS.toSeconds(timeLeft) > CooldownManager.DEFAULT_COOLDOWN || CooldownManager.DEFAULT_COOLDOWN == -1){
+                        player.openInventory(inv);
+                    }else{
+                        if (cooldownMessage.length() > 1) {
+                            long seconds = (TimeUnit.MILLISECONDS.toSeconds(timeLeft) - CooldownManager.DEFAULT_COOLDOWN);
+                            String strip = String.valueOf(seconds).replace("-", "");
+                            String message = cooldownMessage.replace("{time}", strip);
+                            player.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
+                        }
                     }
+                    return true;
                 }
-                return true;
             }
         }
         return false;
