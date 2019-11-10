d31785ce6c8714629f128e011dc8d3fa4cdc6ac2
Stian Tofte <stitofte@outlook.com>
2019-11-11 00:39:27 +0100

    Didn't need to change this afterall. Just grabbing pr world enabled.

--- src/main/java/com/ohneemc/OhWild/util/CooldownManager.java
+++ src/main/java/com/ohneemc/OhWild/util/CooldownManager.java
@@ -9,16 +9,19 @@ public class CooldownManager {
     private static final Map<UUID, Long> cooldowns = new HashMap<>();
 
     public static final int DEFAULT_COOLDOWN = Config.getInteger("settings.cooldown");
+    public static final boolean DEFAULT_PRWORLD = Config.getBoolean("settings.prworld");
 
-    public static void setCooldown(UUID player, long time){
-        if(time < 1) {
-            cooldowns.remove(player);
-        } else {
-            cooldowns.put(player, time);
+    public static void setCooldown(UUID player, long time) {
+        {
+            if (time < 1) {
+                cooldowns.remove(player);
+            } else {
+                cooldowns.put(player, time);
+            }
         }
     }
 
-    public static long getCooldown(UUID player){
+    public static long getCooldown(UUID player) {
         return cooldowns.getOrDefault(player, 0L);
     }
 }
