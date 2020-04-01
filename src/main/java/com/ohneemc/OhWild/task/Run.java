3fc6b2e9a9d7904ed5882cefcd107611965aaca9
Stian Tofte <stian@realnaits.com>
2020-04-01 22:10:08 +0200

    Removed public

--- src/main/java/com/ohneemc/OhWild/task/Run.java
+++ src/main/java/com/ohneemc/OhWild/task/Run.java
@@ -3,9 +3,9 @@ package com.ohneemc.OhWild.task;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
-public class Run {
+class Run {
 
-    public static void runWild(Player player, Location location) {
+    static void runWild(Player player, Location location) {
         player.teleport(location.add(0,0.2,0));
     }
 }
