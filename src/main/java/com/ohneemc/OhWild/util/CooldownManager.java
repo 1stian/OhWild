be2baa6a06ffebfca9378f8a2725837631dcfd6f
Stian Tofte <stian@realnaits.com>
2020-04-11 17:01:01 +0200

    Setting pr.world to false. It would break cd if true.

--- src/main/java/com/ohneemc/OhWild/util/CooldownManager.java
+++ src/main/java/com/ohneemc/OhWild/util/CooldownManager.java
@@ -1,7 +1,6 @@
 package com.ohneemc.OhWild.util;
 
 import org.bukkit.Bukkit;
-import org.bukkit.World;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,9 +10,12 @@ public class CooldownManager {
 
     private static final Map<UUID, Long> cooldowns = new HashMap<>();
     private static final Map<UUID, String> worldCooldown = new HashMap<>();
+    private static final Map<String, Long> worldSeconds = new HashMap<>();
 
     public static final int DEFAULT_COOLDOWN = Config.getInteger("settings.cooldown");
-    public static final boolean DEFAULT_PRWORLD = Config.getBoolean("settings.prworld");
+    //public static final boolean DEFAULT_PRWORLD = Config.getBoolean("settings.prworld");
+
+    public static final boolean DEFAULT_PRWORLD = false;
 
     public static void setCooldown(UUID player, long time) {
         {
@@ -37,5 +39,11 @@ public class CooldownManager {
 
     public static String getWorld(UUID player) {
         return worldCooldown.get(player);
+    }
+
+    public static long getWorldCooldown(UUID player,String wName){
+
+
+        return worldSeconds.get(wName);
     }
 }
